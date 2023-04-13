91da845208ed215724e64a9862c2a300dcb5a3f9
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-04-14 02:10:49 +0700

    firstday-addbot&slash command

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config(); // give access to env var
+const { Client, IntentsBitField } = require("discord.js"); //Client ==> bot , Intent ==> what our bot can do
+
+const client = new Client({
+  // set action
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+}); //Guild = server
+
+client.on("ready", (c) => {
+  console.log(`${c.user.tag} is online 🤩`);
+}); //listner when bot is ready and log out
+
+client.on("messageCreate", (message) => {
+  // event when type message
+  if (message.author.bot) {
+    //prevent bot not reply yourself
+    return;
+  }
+  if (message.content === "hello" || "hi") {
+    // reply message
+    message.reply("Hey! 🤓");
+  }
+});
+
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "hey") {
+    interaction.reply("hey👋");
+  }
+  if (interaction.commandName === "ping") {
+    interaction.reply("Pong!💥");
+  }
+});
+
+client.login(process.env.TOKEN); // login bot into server [online bot]
---
+++ src/register-commands.js
@@ -0,0 +1,30 @@
+require("dotenv").config(); // give access to env var
+const { REST, Routes } = require("discord.js"); // import
+const commands = [
+  //define command
+  {
+    name: "hey",
+    description: "Replies with hey!",
+  },
+  {
+    name: "ping",
+    description: "Pong!",
+  },
+];
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Registering slash commands...");
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+    console.log("Slash commands weew registered successfully!");
+  } catch (error) {
+    console.log(`There was an error : ${error}`);
+  }
+})(); //register / command
